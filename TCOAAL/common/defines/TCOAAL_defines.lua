8df63202cdf6cfa8e999088adda018aaaef22b06
TheGamerCant <123405360+TheGamerCant@users.noreply.github.com>
2023-12-22 16:36:18 +0000

    Doesn't load but I don't care

---
+++ TCOAAL/common/defines/TCOAAL_defines.lua
@@ -0,0 +1,3 @@
+
+NDefines.NGame.GAME_SPEED_SECONDS = { 0.2, 0.0, 0.0, 0.0, 0.0 }			--Every hour in-game is .2 seconds irl
+
---
+++ TCOAAL/descriptor.mod
@@ -0,0 +1,8 @@
+version="1.0"
+tags={
+	"Utilities"
+	"Gameplay"
+	"Graphics"
+}
+name="TCOAAL"
+supported_version="1.13.*"
